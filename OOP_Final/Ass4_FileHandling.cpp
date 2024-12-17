893a3186b346de5a6a32f69aabedd8aca0eda2d2
Shreeram Kulkarni <p03749129@gmail.com>
2024-12-17 22:24:25 +0530

    Update Ass4_FileHandling.cpp

--- OOP_Final/Ass4_FileHandling.cpp
+++ OOP_Final/Ass4_FileHandling.cpp
@@ -10,50 +10,48 @@ class student
 
     void getdata()
     {
-        cout<<"Entr Name :- ";
+        cout<<"Enter Name :- ";
         cin>>name;
-        cout<<"Enetr Roll No :- ";
+        cout<<"Enter Roll No :- ";
         cin>>rollno;
         cout<<"Enter Div:- ";
         cin>>div;
     }
     void show()
     {
-        cout<<"Name:- "<<name;
-        cout<<"Roll No:- "<<rollno;
-        cout<<"Division:- "<<div;
+        cout<<"Name:- "<<name<<endl;
+        cout<<"Roll No:- "<<rollno<<endl;
+        cout<<"Division:- "<<div<<endl;
     }
 };
 
-int main (){
-
-int n;
-cout<<"How Manu Number OF Students:- ";
-cin>>n;
-
-fstream file;
-//file.open("C:\Users\Admin\Desktop\tut.txt",ios::out);
-
-student s[10];
-for(int i=0;i<n;i++)
+int main ()
 {
-    s[i].getdata();
-    file.write((char*)&s[i],sizeof s[i]);
+
+    int n;
+    cout<<"How Many Number OF Students:- ";
+    cin>>n;
+
+    fstream file;
+    file.open("C:\\Users\\Admin\\Desktop\\tut.txt",ios::out);
+
+    student s[10];
+    for(int i=0;i<n;i++)
+    {
+        s[i].getdata();
+        file.write((char*)&s[i],sizeof s[i]);
+    }
+    cout<<endl;
+    file.close();
+    file.open("C:\\Users\\Admin\\Desktop\\tut.txt",ios::in);
+    for(int i=0;i<n;i++)
+    {
+        file.read((char*)&s[i],sizeof s[i]);
+        s[i].show();
+    }
+    file.close();
+
+        
+
+    return 0;
 }
-cout<<endl;
-file.close();
-
-for(int i=0;i<n;i++)
-{
-    file.read((char*)&s[i],sizeof s[i]);
-    s[i].show();
-}
-file.close();
-
-    
-
-  return 0;
-}
-
-
-
